5f29db8f061873b809eb1354ce2db44983ae4554
Ioannis Antoniou <johnantony24@hotmail.com>
2025-05-14 23:19:16 +0200

    bookpage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,4 +10,29 @@ class BooksController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Book not found' }, status: :not_found
   end
+
+  def create
+  @book = Book.new(book_params)
+  if @book.save
+    render json: @book, status: :created
+  else
+    render json: { error: @book.errors.full_messages.join(", ") }, status: :unprocessable_entity
+  end
+end
+
+def update
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
+    render json: @book
+  else
+    render json: { error: @book.errors.full_messages.join(", ") }, status: :unprocessable_entity
+  end
+end
+
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :description, :cover_url, :progress)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :guestbook, only: [:index, :create], controller: 'guestbook_entries'
     get 'visitor_count', to: 'visitor_counts#show'
     post 'visitor_count/increment', to: 'visitor_counts#increment'
-    resources :books, only: [:index, :show]
+    resources :books, only: [:index, :show, :create, :update]
     resources :posts, only: [:index, :show, :create]
     resources :contacts, only: [:create]
   end
